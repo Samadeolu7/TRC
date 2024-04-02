81f14ecdcde5f12156746c61f4532e8905f7c6a0
Samuel <samadeolu7@gmail.com>
2024-04-02 14:44:40 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,233 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, marshal_with, fields
+from models import LiveService, live_services_schema,MajorService,Event,Sermons,MajorEvents
+from models import db
+from flask_jwt_extended import JWTManager, jwt_required
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
+
+api = Api(app)
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'description': fields.String,
+    'url': fields.String,
+    'is_active': fields.Boolean
+}
+
+class LiveServiceList(Resource):
+    def get(self):
+        live_services = LiveService.query.all()
+        return live_services_schema.dump(live_services)
+    
+     
+    @marshal_with(resource_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('url', type=str, required=True)
+        parser.add_argument('is_active', type=bool, required=True)
+        data = parser.parse_args()
+        
+        new_live_service = LiveService(
+            name=data['name'],
+            description=data['description'],
+            url=data['url'],
+            is_active=data['is_active']
+        )
+        db.session.add(new_live_service)
+        db.session.commit()
+        return live_services_schema.dump(new_live_service)
+    
+     
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        data = parser.parse_args()
+        
+        live_service = LiveService.query.get(data['id'])
+        if live_service:
+            db.session.delete(live_service)
+            db.session.commit()
+            return {
+                'message': 'The live service has been deleted'
+            }
+        return {
+            'message': 'The live service does not exist'
+        }
+    
+     
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('url', type=str, required=True)
+        parser.add_argument('is_active', type=bool, required=True)
+        data = parser.parse_args()
+        
+        live_service = LiveService.query.get(data['id'])
+        if live_service:
+            live_service.name = data['name']
+            live_service.description = data['description']
+            live_service.url = data['url']
+            live_service.is_active = data['is_active']
+            db.session.commit()
+            return live_services_schema.dump(live_service)
+        return {
+            'message': 'The live service does not exist'
+        }
+    
+class MajorEventsList(Resource):
+    def get(self):
+        major_events = MajorEvents.query.all()
+        return live_services_schema.dump(major_events)
+    
+     
+    @marshal_with(resource_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('image', type=str, required=True)
+        parser.add_argument('list_of_guests', type=str, required=True)
+        parser.add_argument('date', type=str, required=True)
+        parser.add_argument('url', type=str, required=True)
+        data = parser.parse_args()
+        
+        new_major_event = MajorEvents(
+            name=data['name'],
+            description=data['description'],
+            image=data['image'],
+            list_of_guests=data['list_of_guests'],
+            date=data['date'],
+            url=data['url']
+        )
+        db.session.add(new_major_event)
+        db.session.commit()
+        return live_services_schema.dump(new_major_event)
+    
+     
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        data = parser.parse_args()
+        
+        major_event = MajorEvents.query.get(data['id'])
+        if major_event:
+            #delete all guests
+            for guest in major_event.list_of_guests:
+                db.session.delete(guest)
+            db.session.delete(major_event)
+            db.session.commit()
+            return {
+                'message': 'The major event has been deleted'
+            }
+        return {
+            'message': 'The major event does not exist'
+        }
+    
+     
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('image', type=str, required=True)
+        parser.add_argument('list_of_guests', type=str, required=True)
+        parser.add_argument('date', type=str, required=True)
+        parser.add_argument('url', type=str, required=True)
+        data = parser.parse_args()
+        
+        major_event = MajorEvents.query.get(data['id'])
+        if major_event:
+            major_event.name = data['name']
+            major_event.description = data['description']
+            major_event.image = data['image']
+            major_event.list_of_guests = data['list_of_guests']
+            major_event.date = data['date']
+            major_event.url = data['url']
+            db.session.commit()
+
+class UpcomingServicesList(Resource):
+    def get(self):
+        upcoming_services = MajorService.query.all()
+        return live_services_schema.dump(upcoming_services)
+    
+     
+    @marshal_with(resource_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('day', type=str, required=True)
+        parser.add_argument('time', type=str, required=True)
+        data = parser.parse_args()
+        
+        new_upcoming_service = MajorService(
+            name=data['name'],
+            description=data['description'],
+            day=data['day'],
+            time=data['time']
+        )
+        db.session.add(new_upcoming_service)
+        db.session.commit()
+        return live_services_schema.dump(new_upcoming_service)
+    
+     
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        data = parser.parse_args()
+        
+        upcoming_service = MajorService.query.get(data['id'])
+        if upcoming_service:
+            db.session.delete(upcoming_service)
+            db.session.commit()
+            return {
+                'message': 'The upcoming service has been deleted'
+            }
+        return {
+            'message': 'The upcoming service does not exist'
+        }
+    
+     
+    @marshal_with(resource_fields)
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('day', type=str, required=True)
+        parser.add_argument('time', type=str, required=True)
+        data = parser.parse_args()
+        
+        upcoming_service = MajorService.query.get(data['id'])
+        if upcoming_service:
+            upcoming_service.name = data['name']
+            upcoming_service.description = data['description']
+            upcoming_service.day = data['day']
+            upcoming_service.time = data['time']
+            db.session.commit()
+            return live_services_schema.dump(upcoming_service)
+        return {
+            'message': 'The upcoming service does not exist'
+        }
+    
+api.add_resource(LiveServiceList, '/liveservices')
+api.add_resource(MajorEventsList, '/majorevents')
+api.add_resource(UpcomingServicesList, '/upcomingevents')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,126 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+
+
+class LiveService(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    url = db.Column(db.String(200))
+    is_active = db.Column(db.Boolean)
+
+class MajorService(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    day = db.Column(db.String(200))
+    time = db.Column(db.String(200))
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    date = db.Column(db.String(200))
+
+class Sermons(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    date = db.Column(db.String(200))
+    url = db.Column(db.String(200))
+    type = db.Column(db.String(200))
+    #sort by date
+
+class MajorEvents(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+    date = db.Column(db.String(200))
+    url = db.Column(db.String(200))
+    guests = db.relationship('Guest', backref='major_event')
+
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+    major_event_id = db.Column(db.Integer, db.ForeignKey('major_events.id'))
+
+
+class LiveServiceSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'date', 'url', 'is_active')
+
+live_service_schema = LiveServiceSchema()
+live_services_schema = LiveServiceSchema(many=True)
+
+
+
+#create dummy database
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        new_live_service = LiveService(
+            name='Sunday Service',
+            description='Sunday Service',
+            url='https://www.youtube.com/watch?v=8c7B2v1b5wQ',
+            is_active=True
+        )
+        db.session.add(new_live_service)
+        db.session.commit()
+
+        new_live_service = LiveService(
+            name='Wednesday Service',
+            description='Wednesday Service',
+            url='https://www.youtube.com/watch?v=8c7B2v1b5wQ',
+            is_active=True
+        )
+        db.session.add(new_live_service)
+        db.session.commit()
+
+        new_live_service = LiveService(
+            name='Friday Service',
+            description='Friday Service',
+            url='https://www.youtube.com/watch?v=8c7B2v1b5wQ',
+            is_active=True
+        )
+        db.session.add(new_live_service)
+        db.session.commit()
+
+        new_major_event = MajorEvents(
+            name='Youth Conference',
+            description='Youth Conference',
+            image='https://www.youtube.com/watch?v=8c7B2v1b5wQ',
+            date='2020-12-25',
+            url='https://www.youtube.com/watch?v=8c7B2v1b5wQ'
+        )
+        db.session.add(new_major_event)
+        db.session.commit()
+
+        new_major_event = MajorEvents(
+            name='Youth Conference',
+            description='Youth Conference',
+            image='https://www.youtube.com/watch?v=8c7B2v1b5wQ',
+            date='2020-12-25',
+            url='https://www.youtube.com/watch?v=8c7B2v1b5wQ'
+        )
+        db.session.add(new_major_event)
+        db.session.commit()
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
