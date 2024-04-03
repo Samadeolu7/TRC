bf5304f4ce3f54a1e6bcb3e3e4486b2a0e91c7f0
Samuel <samadeolu7@gmail.com>
2024-04-03 21:22:38 +0100

    made Api guide a class

--- app.py
+++ app.py
@@ -225,33 +225,34 @@ class UpcomingServicesList(Resource):
         }
     
 @app.route('/api-guide', methods=['GET'])
-def api_guide():
-    guide = {
-        "/liveservices": {
-            "GET": "Returns a list of all live services",
-            "POST": "Creates a new live service",
-            "DELETE": "Deletes a live service",
-            "PUT": "Updates a live service"
-        },
-        "/majorevents": {
-            "GET": "Returns a list of all major events",
-            "POST": "Creates a new major event",
-            "DELETE": "Deletes a major event",
-            "PUT": "Updates a major event"
-        },
-        "/upcomingevents": {
-            "GET": "Returns a list of all upcoming services",
-            "POST": "Creates a new upcoming service",
-            "DELETE": "Deletes an upcoming service",
-            "PUT": "Updates an upcoming service"
+class ApiGuide(Resource):
+    def get(self):
+        guide = {
+            "/liveservices": {
+                "GET": "Returns a list of all live services",
+                "POST": "Creates a new live service",
+                "DELETE": "Deletes a live service",
+                "PUT": "Updates a live service"
+            },
+            "/majorevents": {
+                "GET": "Returns a list of all major events",
+                "POST": "Creates a new major event",
+                "DELETE": "Deletes a major event",
+                "PUT": "Updates a major event"
+            },
+            "/upcomingevents": {
+                "GET": "Returns a list of all upcoming services",
+                "POST": "Creates a new upcoming service",
+                "DELETE": "Deletes an upcoming service",
+                "PUT": "Updates an upcoming service"
+            }
         }
-    }
-    return guide
+        return guide
     
 api.add_resource(LiveServiceList, '/liveservices')
 api.add_resource(MajorEventsList, '/majorevents')
 api.add_resource(UpcomingServicesList, '/upcomingevents')
-api.add_resource(api_guide, '/api-guide')
+api.add_resource(ApiGuide, '/api-guide')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 ma = Marshmallow(app)
