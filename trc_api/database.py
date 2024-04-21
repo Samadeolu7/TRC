b90dc83017ec897517380d515af0cc46b5abc9da
Samuel <samadeolu7@gmail.com>
2024-04-21 09:46:14 +0100

    made database to depend on env

--- trc_api/database.py
+++ trc_api/database.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 db = SQLAlchemy()
 db.init_app(app)
