f87b05999caa4a339d5ffb8c8967b794305b5b30
Samuel <samadeolu7@gmail.com>
2024-04-24 17:38:48 +0100

    fixed app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from flask import Flask, request, jsonify
-
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>TRC API</h1><p>This site is a prototype API for TRC.</p>"
+from run import app
 
 
 if __name__ == "__main__":
