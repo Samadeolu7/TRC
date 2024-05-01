cc04b26e5c5a2422c39b3145a28e75f95950a836
Samuel <samadeolu7@gmail.com>
2024-05-01 18:52:05 +0100

    added api guide

--- trc_api/ping/app.py
+++ trc_api/ping/app.py
@@ -6,6 +6,17 @@ class Ping(Resource):
     def get(self):
         return {'message': 'Pong!'}, 200
 
+class ApiGuide(Resource):
+    def get(self):
+        guide = {
+            'ping': 'Check if the server is up',
+            'upcoming_services': 'Get all upcoming services',
+            'upcoming_events': 'Get all upcoming events',
+            'major_events': 'Get all major events',     
+        }
+        return jsonify(guide)
+    
 ping_bp = blueprints.Blueprint('ping', __name__)
 api = Api(ping_bp)
 api.add_resource(Ping, '/ping')
+api.add_resource(ApiGuide, '/api_guide')
